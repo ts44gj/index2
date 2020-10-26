020fd6fd43770adce6a9d8b90809aa3dda1b537e
ts44gj <ts44gj2020@gmail.com>
2020-10-26 11:11:47 +0900

    saaa

---
+++ script.js
@@ -0,0 +1,8 @@
+function displyon1(){
+    document.getElementById("demo").innerHTML = "生年月日:平成３年4月4日";} 
+function displyon2(){
+    document.getElementById("demo").innerHTML = "プログラミング習得";}
+function displyon3(){
+    document.getElementById("demo").innerHTML = "筋トレ";}
+function displyoff(){
+        document.getElementById("demo").innerHTML = "";}  
